3751790e6a871d669e1d34a0d1f5b4f2840cdb2f
Aleksei Margolin <margolin96@gmail.com>
2018-11-10 02:40:13 +0300

    vue-router component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 window.mapboxgl = require('mapbox-gl')
 
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
 
 import '@denull/vkui/src/styles/generated/palette.css';
