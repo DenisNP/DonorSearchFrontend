ff079d97884e858a183cf5cfa53f1728ad1c6fd9
Aleksei Margolin <margolin96@gmail.com>
2018-11-10 00:02:35 +0300

    UserProfile updated

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,24 +1,46 @@
 <template>
     <div class="UserProfile">
-        <VKView v-bind="$attrs" activePanel="panel">
-            <Panel id="panel">
-                <PanelHeader>{{title}}</PanelHeader>
+        <VKView v-bind="$attrs" :activePanel="activePanel">
+            <Panel id="Profile">
+                <PanelHeader>Профиль</PanelHeader>
 
-                <FormLayout>
-                    <SelectMimicry
-                        top="Выберите город"
-                        placeholder="Не выбран"
-                        :onClick="cityPopupOpen"
-                    >
-                        {{selectedCityName}}
-                    </SelectMimicry>
-                </FormLayout>
+                <Group title="Кто ты?">
+                    <Div>
+                        <pre>{{profile}}</pre>
+                    </Div>
+                </Group>
+
+                <Group title="Где сдавать?">
+                    <List>
+                        <Cell expandable :onClick="citySelectionOpen">
+                            <vkui-icon :size="24" name="place" slot="before" />
+                            {{selectedCityName}}
+                        </Cell>
+                    </List>
+                </Group>
+
+                <Group title="Отладка">
+                    <Div>
+                        <Button @click="profile.city.name = 'Test'">Сменить город</Button>
+                    </Div>
+                </Group>
             </Panel>
-        </VKView>
-        <VKView id="citySelectionView" activePanel="panel" key="citySelectionView">
-            <Panel id="panel">
-                <PanelHeader>Выбор города</PanelHeader>
-                test
+            <Panel id="CitySelection">
+                <PanelHeader noShadow>
+                    <HeaderButton key="add" slot="right">
+                        <vkui-icon :size="24" name="add" />
+                    </HeaderButton>
+
+                    Выбор города
+                </PanelHeader>
+
+                <Search :value="CitySelection.search" :onChange="CitySelectionChange" />
+
+                <List>
+                    <Cell v-for="item in CitySelection.list" :key="item.id">
+                        {{item.name}}
+                    </Cell>
+                </List>
             </Panel>
         </VKView>
     </div>
@@ -27,29 +49,30 @@
 <script>
 
 import '@denull/vkui'
+import dsApi from '../DSApi'
 
 export default {
     name: 'UserProfile',
-    props: {
-        title: String
-    },
+    props: {},
     computed: {
         selectedCityName() {
-            if (this.profile && this.profile.city) {
+            if (this.profile && this.profile.city && this.profile.city.name) {
                 return this.profile.city.name;
             }
 
-            return '';
+            return 'Город не выбран';
         }
+    },
+    mounted() {
+        dsApi.send('users/1910986', {}, (data) => {
+            this.profile = data
+        })
     },
     data() {
         return {
-            activeView: 'profile',
+            activePanel: 'Profile',
             profile: {
-                city: {
-                    id: 0,
-                    name: 'Санкт-Петербург'
-                }
+                city: {}
             },
             cities: [
                 {
@@ -62,12 +85,23 @@ export default {
                     id: 3,
                     name: 'Казань'
                 }
-            ]
+            ],
+
+            CitySelection: {
+                search: '',
+                list: []
+            }
         }
     },
     methods: {
-        cityPopupOpen() {
-            this.activeView = 'citySelectionView'
+        citySelectionOpen() {
+            this.activePanel = 'CitySelection'
+        },
+
+        CitySelectionChange(e) {
+            this.CitySelection.list.push(this.cities[1]);
+            this.CitySelection.list.push(this.cities[2]);
+            this.CitySelection.list.push(this.cities[3]);
         }
     }
 }
@@ -77,9 +111,6 @@ export default {
 <style>
 
 .UserProfile {
-    border: 1px solid #E5E5E5;
-    width: 400px;
-    margin: 40px auto;
     text-align: left;
 }
 
