8636cf31da3b7a3a8ca910d3a4c049af37c6e05e
Aleksei Margolin <margolin96@gmail.com>
2018-11-11 04:15:37 +0300

    WarningView

---
+++ src/components/WarningView.vue
@@ -0,0 +1,45 @@
+<template>
+	<Panel id="warnings">
+		<PanelHeader>
+			<HeaderButton slot="left" @click="WarningsClose">
+                <template v-if="osname === 'IOS'">
+                    Отмена
+                </template>
+                <vkui-icon :size="24" name="cancel" v-else />
+            </HeaderButton>
+
+			Противопоказания
+		</PanelHeader>
+
+		<Group>
+			<List>
+				<Cell title="Не есть!" description="Никогда!">
+					<vkui-icon :size="24" name="error" slot="before" />
+				</Cell>
+			</List>
+		</Group>
+	</Panel>
+</template>
+
+<script>
+
+export default {
+	components: {
+		Panel, Group, List, Cell, PanelHeader, HeaderButton
+	},
+	name: 'WarningView',
+	data() {
+		return {
+
+		}
+	},
+	methods: {
+		
+	}
+}
+
+</script>
+
+<style>
+
+</style>
