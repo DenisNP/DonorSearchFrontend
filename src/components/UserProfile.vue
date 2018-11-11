e1d3ed9c41d8bb512be9c107b761f2db79c6ac3c
Aleksei Margolin <margolin96@gmail.com>
2018-11-11 07:44:27 +0300

    Fix bdate parse bug

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -236,9 +236,9 @@ export default {
         DSProfile(val) {
             let date = new Date(val.bdate);
             this.DSProfileBDate = [
-                date.getFullYear(),
-                date.getMonth() + 1,
-                date.getDate()
+                date.getFullYear()
+                ('0' + (date.getMonth() + 1)).slice(-2)
+                ('0' + date.getDate()).slice(-2)
             ].join('-');
 
             this.DSProfileNotFirst = !this.DSProfile.is_first_donor;
